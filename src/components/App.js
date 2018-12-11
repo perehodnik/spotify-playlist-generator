ad5a0024abd84813ff355ed70a1cb13403dbe24b
Maxat Abishev <max.abishev@gmail.com>
2018-12-11 00:03:47 +0000

    remove redundant api folder

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,6 @@ import { connect } from 'react-redux';
 import UserInfo from './UserInfo';
 import ArtistList from './ArtistList';
 import Playlists from './Playlists';
-import * as apiCalls from '../api/api';
 
 
 const spotifyApi = new SpotifyWebApi();
@@ -28,7 +27,6 @@ class App extends Component {
   componentDidMount(){
     if (this.token) {
       this.props.dispatch(setLoginStatus(this.token));
-      apiCalls.setTokenHeader(this.token);
     }
   }
   getHashParams() {
